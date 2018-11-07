30e5820f69765e9cf831b463cc17a56e154e3917
Jiangqiu Shen <jshen2@mtu.edu>
2018-11-07 13:00:17 -0500

    1

--- getGpuInfo.sh
+++ getGpuInfo.sh
@@ -2,12 +2,18 @@ test ! -d cons && mkdir cons;
 test ! -d cons/con && mkdir cons/con;
 test ! -d cons/bypass && mkdir cons/bypass;
 apps=`cat apps.txt`
+action=$1
+if [ "$action" == "clean" ];then
+    echo "clean";
+fi;
+
 
 for app in $apps;
 do
     isEnter="1";
     test -d $app && isEnter="0";  
     if [ $isEnter == "0" ];then
+        echo "entering $app";
         cd $app;
         cp con.txt ../cons/con/con_$app.txt;
         cp bypass_stat.txt ../cons/bypass/bypass_$app.txt;
