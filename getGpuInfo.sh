42df6867a5c61dffca40f9ae6449c766ba1ee10e
Jiangqiu Shen <jshen2@mtu.edu>
2018-11-07 11:20:10 -0500

    add getGpuInfo

--- getGpuInfo.sh
+++ getGpuInfo.sh
@@ -1,8 +1,20 @@
+test ! -d cons && mkdir cons;
+test ! -d cons/con && mkdir cons/con;
+test ! -d cons/bypass && mkdir cons/bypass;
 apps=`cat apps.txt`
 
 for app in $apps:
 do
     cd $app;
+    isEnter=$?;
+    
+    if [ $isEnter == "0" ];then
+        cp con.txt ../cons/con/con_$app.txt;
+        cp bypass_stat.txt ../cons/bypass/bypass_$app.txt;
+        cd ..;
+    else
+        echo "fail to enter";
+    fi
     
 done
 
